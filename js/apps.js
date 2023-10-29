d1f8ae52dd288ec3f9f4f1d5129c37105935fb8d
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-10-29 15:03:09 -0300

    feat: adiciona configuração inicial de rotas para adicionar, editar e excluir usuários

--- js/apps.js
+++ js/apps.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Olá, mundo!');
 });
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Aplicativo rodando na porta ${port}`);
+
+router.get('/', (req, res) => {
+
+})
+
+router.post('/users', (req, res) => {
+
+})
+
+// Rota para excluir um usuário (por exemplo)
+app.delete('/users/:id', (req, res) => {
+    const userId = req.params.id; // Obtém o ID do usuário da URL
+    // Lógica para excluir o usuário com o ID fornecido
+    // Isso pode envolver interação com um banco de dados ou outro armazenamento
+    res.send(`Usuário com ID ${userId} excluído com sucesso.`);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
 });
